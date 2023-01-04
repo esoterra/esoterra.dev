ba256f24fc0def2dac0504e883638ab7390f1b73
Kyle Brown <kylebrw@gmail.com>
2023-01-03 23:50:46 -0500

    Add prism.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 const pluginSEO = require("eleventy-plugin-seo");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ static: "/" });
+
+    eleventyConfig.addPlugin(syntaxHighlight);
 
     eleventyConfig.addPlugin(pluginSEO, {
         title: "Kyle B",
